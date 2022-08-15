5577b69cd5a64d00e1b3562b75cfca9893db3b97
Sergio18rg <Sergio18rg@gmail.com>
2022-08-15 22:37:35 +0200

    Navbar component

---
+++ src/components/NavBar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface INavbarProps {
+    links: Array<Object>,
+}
+
+const Navbar = ({links}:INavbarProps) => {
+
+    return (
+        <h1>Sergio RG</h1>
+    )
+}
+
+export default Navbar
